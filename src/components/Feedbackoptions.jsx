a2d5446934b76e11f2661b56cc8b7e517ef69c1e
petruvlad <vlad.petrisor31@gmail.com>
2023-11-21 15:55:08 +0200

    final commit
    functioneaza aplicatia

---
+++ src/components/Feedbackoptions.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map(option => (
+        <button key={option} onClick={() => onLeaveFeedback(option)}>
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
